95af004735a8ae6e61f0e04c333cfc6339882088
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-01-30 00:38:53 +0100

    delete  element from localStorage

--- script.js
+++ script.js
@@ -48,17 +48,17 @@ clickButton.addEventListener('click', function(){
 // Desplay task
  function DisplayTask(){
      list.innerHTML = "";
-     taskList.forEach(element => {
+     taskList.forEach((element, index) => {
         const li = document.createElement("li");
         li.innerHTML = `
          <div class="flex items-center justify-between p-4 bg-gray-50 rounded-lg shadow">
                 <div>
-                    <h3 class="font-bold" data-id="${element.id}"> ${element.nameTaskValue}</h3>
+                    <h3 class="font-bold"> ${element.nameTaskValue}</h3>
                     <span class="text-sm text-gray-500">priority: ${element.statutTaskValue}</span>
                 </div>
                 <div class="flex items-center space-x-2">
                     <button class="px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600"><i class='bx bxs-edit-alt'></i></button>
-                    <button class=" remove px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"><i class='bx bxs-trash-alt' ></i></button>
+                    <button onclick= "deletTask(${index})" class=" remove px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"><i class='bx bxs-trash-alt' ></i></button>
                 </div>
             </div>
         `;
@@ -71,7 +71,7 @@ clickButton.addEventListener('click', function(){
  }
 
  function displayTaskCompleted(){
-    taskList.forEach(element =>{
+    taskList.forEach((element, index) =>{
         const li = document.createElement("li");
         if(element.statutTaskValue == 'Done'){
             li.innerHTML = `
@@ -85,8 +85,15 @@ clickButton.addEventListener('click', function(){
  //function delete
 // Fonction pour supprimer une tâche
 function deletTask(index) {
-    taskList.splice(index, 1); 
-    localStorage.setItem("taskList", JSON.stringify(taskList));
+     var taskList;
+     if(localStorage.getItem("taskList") == null){
+        taskList = [];
+     }else{
+        taskList= JSON.parse(localStorage.getItem("taskList"));
+     }
+     taskList.splice(index, 1);
+     localStorage.setItem("taskList", JSON.stringify(taskList));
+     DisplayTask();
  }
 
 
